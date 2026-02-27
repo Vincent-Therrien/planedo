32edd8d1220314574af5da31aaef1c5a83fd21b6
Vincent Therrien <vincentstherrien@gmail.com>
2026-02-26 21:08:05 -0500

    Script

--- data/filter_exoplanets.py
+++ data/filter_exoplanets.py
@@ -1,10 +1,23 @@
-from __future__ import annotations
-
 import csv
+import json
 from pathlib import Path
 
-INPUT_FILE = Path(__file__).with_name("confirmed_exoplanets.csv")
-OUTPUT_FILE = Path(__file__).with_name("confirmed_exoplanets_filtered.csv")
+ALL_FILE = Path(__file__).with_name("all_exoplanets.csv")
+OUTPUT = Path(__file__).with_name("exoplanets.json")
+
+star_systems = {}
+unknown_distance = []
+star_system_entry = {
+    "RA": float,
+    "Dec": float,
+    "Distance": float,
+    "Planets": list,
+}
+planet_entry = {
+    "Confirmed": bool,
+    "Mass (Earth)": float,
+    "Discovery year": int,
+}
 
 
 def _iter_data_lines(csv_path: Path):
@@ -15,20 +28,42 @@ def _iter_data_lines(csv_path: Path):
             yield line
 
 
-def main() -> None:
-    reader = csv.DictReader(_iter_data_lines(INPUT_FILE))
-    with OUTPUT_FILE.open("w", newline="", encoding="utf-8") as target:
-        writer = csv.writer(target)
-        writer.writerow(["system_name", "planet_name", "ra", "dec", "distance"])
-        planets = set()
-        for row in reader:
-            planet_name = (row.get("pl_name") or "").strip()
-            system_name = (row.get("hostname") or "").strip()
-            ra = row.get("ra").strip()
-            dec = row.get("dec").strip()
-            distance = row.get("sy_dist").strip()
-            writer.writerow([system_name, planet_name, ra, dec, distance])
+def process_file(filename: Path) -> None:
+    reader = csv.DictReader(_iter_data_lines(filename))
+    for row in reader:
+        system_name = row.get("hostname")
+        if system_name not in star_systems:
+            try:
+                distance = float(row.get("sy_dist").strip())
+            except:
+                distance = "Unknown"
+                unknown_distance.append(system_name)
+            star_systems[system_name] = {
+                "RA (deg)": float(row.get("ra").strip()),
+                "Dec (deg)": float(row.get("dec").strip()),
+                "Distance (pc)": distance,
+                "Planets": {}
+            }
+        mass = row.get("pl_masse").strip()
+        mass = mass if mass else "Unknown"
+        radius = row.get("pl_rade").strip()
+        radius = radius if radius else "Unknown"
+        link_name = row.get("pl_name").strip().lower().replace(" ", "-")
+        star_systems[system_name]["Planets"][row.get("pl_name").strip()] = {
+            "Mass (Earth mass)": mass,
+            "Radius (Earth radius)": radius,
+            "Discovery year": row.get("disc_year").strip(),
+            "Discovery method": row.get("discoverymethod").strip(),
+            "Catalog link": f"https://science.nasa.gov/exoplanet-catalog/{link_name}",
+        }
 
 
-if __name__ == "__main__":
-    main()
+process_file(ALL_FILE)
+
+with open(OUTPUT, "w") as file:
+    json.dump(star_systems, file, indent=4)
+
+print(f"Number of star systems: {len(star_systems)}")
+print(f"Number of planets: {sum([len(s['Planets']) for s in star_systems.values()])}")
+print(unknown_distance)
+print(len(unknown_distance))
